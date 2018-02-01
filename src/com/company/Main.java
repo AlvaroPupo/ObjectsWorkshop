638649bd0e9b89481dab2754c4de3fd21c613190
AlvaroPupo <juanhernandezpupo@gmail.com>
2018-02-01 14:45:48 -0500

    added pencil object

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//
+//        Cat cat = new Cat();
+//
+//        System.out.println(cat.getColor());
+        pencil Pencil = new pencil(.7,"Dixon",true,"Orange",false);
+        pencil Pencil1 = new pencil(.8,"paper-mate",true,"yellow",false);
+        System.out.println("this is pencil: " + Pencil.getColor());
+        System.out.println("this is pencil 1: " + Pencil1.getColor());
+
+
     }
 }
